2bd61bf36ce8a145bb51c7fed2cb9916d5b3ec36
Christoph Schneider <Christoph.Schneider@dci-student.org>
2025-11-04 13:27:43 +0100

    chore: added psycopg2 to requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'some-crazy-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "doc-distance-api.onrender.com"]
 HOST = os.environ.get('HOST')
 if HOST:
     ALLOWED_HOSTS.append(HOST)
